638d289a6e0170f00a14781252127679d342890a
Sujee Maniyam <sujee@elephantscale.com>
2021-06-09 12:56:22 -0700

    update ps1 run script

--- docker-es-training/run-docker.ps1
+++ docker-es-training/run-docker.ps1
@@ -23,7 +23,7 @@ IF ([string]::IsNullOrWhitespace($image)) {
 
 $working_dir = $(Get-Location)
 
-$data_dir = ${working_dir}/data
+$data_dir = "${working_dir}/data" 
 
 
 $command = "docker run -it -v ${working_dir}:/home/ubuntu/dev -v ${data_dir}:/data  -v ${data_dir}:/home/ubuntu/data -p 80:80 -p 2181:2181 -p 2222:22 -p 3389:3389 -p 4040-4050:4040-4050 -p 6006:6006 -p 8000:8000 -p 8001:8001 -p 8080:8080 -p 8081:8081 -p 8787:8787 -p 8888:8888 -p 9000:9000 -p 9001:9001 -p 9092:9092 -p 10000-10100:10000-10100 -e PASSWORD=${p} ${image} ${cmd}"
